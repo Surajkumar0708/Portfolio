f009c17b58a0ffcbc9ce806ac5eea0e015e00d0b
Surajkumar0708 <srjthakur9@gmail.com>
2024-01-28 01:28:05 +0530

    error fixes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/",
+  basePath: "",
   assetPrefix: "https://main--portfolio2895.netlify.app/playarea",
   //   assetPrefix: "/skills",
   //   assetPrefix: "/project",
