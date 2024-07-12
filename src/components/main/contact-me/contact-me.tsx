cdf82592ac2c611149594b47001d24a0c7a0522d
Surajkumar0708 <srjthakur9@gmail.com>
2024-07-13 00:50:17 +0530

    email integration is completed

--- src/components/main/contact-me/contact-me.tsx
+++ src/components/main/contact-me/contact-me.tsx
@@ -7,10 +7,13 @@ import "./contact.css";
 export default function Contact() {
   const [formData, setFormData] = React.useState({
     name: "",
+    contact: "",
     email: "srjthakur9@gmail.com",
     message: "",
   });
   const [isLoading, setIsLoading] = React.useState(false);
+  const [isEmailSentSuccess, setIsEmailSentSuccess] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   const handleChange = (e: any) => {
     const { name, value } = e.target;
@@ -25,86 +28,117 @@ export default function Contact() {
     setIsLoading(true);
     emailjs
       .send(
-        "service_k7l3de6", // Replace with your EmailJS service ID
-        "template_akh5f7q", // Replace with your EmailJS template ID
+        "service_k7l3de6", // EmailJS service ID
+        "template_loojxeu", // EmailJS template ID
         formData,
-        "hY2fWYFnnyOT9ng3e" // Replace with your EmailJS user ID
+        "hY2fWYFnnyOT9ng3e" // EmailJS user ID
       )
-      .then((response) => {
-        console.log("SUCCESS!", response.status, response.text);
+      .then((response: any) => {
         setIsLoading(false);
-        alert("Email sent successfully!");
+        if (response?.text === "OK") {
+          setIsEmailSentSuccess(true);
+        } else {
+          setIsError(true);
+        }
       })
       .catch((err) => {
-        console.log("FAILED...", err);
-        alert("Failed to send email. Please try again.");
+        setIsLoading(false);
+        setIsError(true);
       });
   };
+
+  const renderForm = () => (
+    <form className="contact_me_form" onSubmit={handleSubmit}>
+      <div>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          placeholder="Your name"
+          style={{
+            width: "100%",
+            padding: "5px",
+            borderRadius: 5,
+            marginBottom: 10,
+            backgroundColor: "#efeff2",
+            border: "none",
+          }}
+          required
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          id="contact"
+          name="contact"
+          value={formData.contact}
+          onChange={handleChange}
+          placeholder="Contact details Email/Phone"
+          style={{
+            width: "100%",
+            padding: "5px",
+            borderRadius: 5,
+            marginBottom: 10,
+            backgroundColor: "#efeff2",
+          }}
+          required
+        />
+      </div>
+      <div>
+        <textarea
+          id="message"
+          name="message"
+          value={formData.message}
+          rows={4}
+          onChange={handleChange}
+          placeholder="Please type your message..."
+          style={{
+            width: "100%",
+            padding: "5px",
+            borderRadius: 5,
+            resize: "vertical",
+            backgroundColor: "#efeff2",
+          }}
+          required
+        ></textarea>
+      </div>
+      <div>
+        <button type="submit" className="btn sendBtn">
+          Send
+        </button>
+      </div>
+    </form>
+  );
+
+  const emailIsProcessing = () => (
+    <div className="formPara">
+      <p>Your Email is sending...</p>
+    </div>
+  );
+
+  const emailSentSuccess = () => (
+    <div className="email-success formPara">
+      <p>Email sent successfully. I'll get in touch with you shortly.</p>
+    </div>
+  );
+
+  const emailError = () => (
+    <div className="email-error formPara">
+      <p>Something went wrong...</p>
+    </div>
+  );
 
   return (
     <div>
       <div className="contactMe">
         <p>{strings.contact}</p>
       </div>
-      <form className="contact_me_form" onSubmit={handleSubmit}>
-        <div>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Your name"
-            style={{
-              width: "100%",
-              padding: "5px",
-              borderRadius: 5,
-              marginBottom: 10,
-              backgroundColor: "#e0e0e0",
-            }}
-            required
-          />
-        </div>
-        <div>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            placeholder="Contact details"
-            style={{
-              width: "100%",
-              padding: "5px",
-              borderRadius: 5,
-              marginBottom: 10,
-              backgroundColor: "#e0e0e0",
-            }}
-            required
-          />
-        </div>
-        <div>
-          <textarea
-            id="message"
-            name="message"
-            value={formData.message}
-            rows={4}
-            onChange={handleChange}
-            placeholder="Please type your message..."
-            style={{
-              width: "100%",
-              padding: "5px",
-              borderRadius: 5,
-              resize: "vertical",
-              backgroundColor: "#e0e0e0",
-            }}
-            required
-          ></textarea>
-        </div>
-        <div>
-          <button>{isLoading ? "Loading" : "Send"}</button>
-        </div>
-      </form>
+      {!isEmailSentSuccess && !isLoading && !isError && renderForm()}
+      {isEmailSentSuccess && emailSentSuccess()}
+      {isLoading && emailIsProcessing()}
+      {isError && emailError()}
     </div>
   );
 }
